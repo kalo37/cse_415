42537a7d87f1cf849810d8e7649fe2089a36f572
Unknown <kenalo37@gmail.com>
2017-11-07 22:05:24 -0800

    a5 init

---
+++ assignment5/loKInARow.py
@@ -0,0 +1,19 @@
+"""Ken Lo
+CSE 415 HW 5
+K In A Row agent"""
+
+def prepare(initial_state, k, what_side_I_play, opponent_nickname):
+    return "OK"
+
+def introduce():
+    return """Hi, my name is Riqroq The Frog, my creator is Ken Lo, his UWID is thlo. I am friendly, \
+    when I'm winning, but when I'm not, you better beware!"""
+
+def nickname():
+    return 'riqroq'
+
+def makeMove(currentState, currentRemark, timeLimit=10000):
+    pass
+
+def staticEval(state):
+    pass
