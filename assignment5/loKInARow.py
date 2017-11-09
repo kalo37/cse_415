3b8d9aada6b581559214eb4ef0b2e5487e773c95
Unknown <kenalo37@gmail.com>
2017-11-08 23:23:57 -0800

    added most of staticEval

--- assignment5/loKInARow.py
+++ assignment5/loKInARow.py
@@ -3,8 +3,9 @@ CSE 415 HW 5
 K In A Row agent"""
 
 INITIAL_STATE = None
-K = None
-MY_SIDE = None
+K = 3
+MY_SIDE = 'X'
+OPP_SIDE = 'O'
 OPP_NICKNAME = None
 
 def prepare(initial_state, k, what_side_I_play, opponent_nickname):
@@ -14,6 +15,11 @@ def prepare(initial_state, k, what_side_I_play, opponent_nickname):
     K = k
     global MY_SIDE
     MY_SIDE = what_side_I_play
+    global OPP_SIDE
+    if what_side_I_play == 'X':
+        OPP_SIDE = 'O'
+    else:
+        OPP_SIDE = 'X'
     global OPP_NICKNAME
     OPP_NICKNAME = opponent_nickname
 
@@ -27,7 +33,76 @@ def nickname():
     return 'riqroq'
 
 def makeMove(currentState, currentRemark, timeLimit=10000):
+    #TODO implement
     pass
 
 def staticEval(state):
-    pass
+    board = state[0]
+    m = len(board)
+    n = len(board[0])
+    my_score = 0
+    opp_score = 0
+
+    #check horizontal
+    for hor in board:
+        try:
+            hor_removed = hor.remove('-')
+        except ValueError:
+            hor_removed = hor
+        my_needed_to_win = testCanWin(hor_removed, MY_SIDE)
+        opp_needed_to_win = testCanWin(hor_removed, OPP_SIDE)
+        if my_needed_to_win > 0:
+            my_score += 10 ** (K - my_needed_to_win)
+        if opp_needed_to_win > 0:
+            opp_score += 10 ** (K - opp_needed_to_win)
+
+    #TODO check vertical
+
+    #TODO check diagonal
+
+    return my_score - opp_score
+
+
+
+def testCanWin(row, side):
+    """return the minimum number of moves needed for the player of "side" to win the row,
+                                    return -1 if the player cannot win the row"""
+    win_steps_list = []
+    for i, cell in enumerate(row):
+        needed_to_win = 0
+        current_consecutive = 0
+
+        if cell == side or cell == ' ':
+            current_consecutive += 1
+            if cell == ' ':
+                needed_to_win += 1
+            if current_consecutive >= K:
+                win_steps_list.append(needed_to_win)
+            else:
+
+                for j in row[i + 1:]:
+                    if j == side or j == ' ':
+                        current_consecutive += 1
+                        if j == ' ':
+                            needed_to_win += 1
+                        if current_consecutive >= K:
+                            win_steps_list.append(needed_to_win)
+                    elif j != side:
+                        current_consecutive = 0
+                        needed_to_win = 0
+
+
+    if win_steps_list != []:
+        return min(win_steps_list)
+    else:
+        return -1
+
+
+INITIAL_STATE = \
+              [[[' ',' ',' '],
+                [' ',' ',' '],
+                [' ',' ',' ']], "X"]
+
+print(staticEval(INITIAL_STATE))
+
+#print(testCanWin([' ', ' ', 'X'], 'O'))
